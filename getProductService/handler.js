698469e45adb6e96321e62af32917091ddb1bad2
ldgonzalez26 <ldgonzalezmedina@gmail.com>
2024-03-30 17:05:10 -0500

    complete task 4
    Signed-off-by: ldgonzalez26 <ldgonzalezmedina@gmail.com>

--- getProductService/handler.js
+++ getProductService/handler.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const Responses = require('./utils/API_Responses');
-const ProductsList = require('./mocks/ProductListMock');
+const Dynamo = require('./utils/Dynamo');
+const productsTableName = process.env.productsTableName;
+const stockTableName = process.env.stockTableName;
 
 module.exports.getProductsById = async (event) => {
   if (!event.pathParameters || !event.pathParameters.productId) {
@@ -9,9 +11,22 @@ module.exports.getProductsById = async (event) => {
   }
 
   let Id = event.pathParameters.productId;
-  let item = ProductsList.find((item) => item.id === Id);
+  const item = await Dynamo.getItem(Id, productsTableName).catch((err) => {
+    console.log(err, 'error in dynamo getProductById');
+    return null;
+  });
   if (item) {
-    return Responses._200(item);
+    const stock = await Dynamo.getStock(Id, stockTableName).catch((err) => {
+      console.log(err, 'error in dynamo getProductById');
+      return null;
+    });
+    if (stock) {
+      item.count = stock.count;
+      return Responses._200(item);
+    } else {
+      item.count = 0;
+      return Responses._200(item);
+    }
   }
 
   return Responses._400({ message: 'Product not found' });
